2928023441ed43d902b3e821773b0a20679f1f2f
Jake Lokkesmoe <jlokkesmoe@me.com>
2015-03-09 23:03:07 -0500

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# CSSE Hub Shell Client
+============
+This is a simple client for the UW Platteville CSSE Hub in the form of a shell script.
+
+## Instalation
+Clone this repository and than run:
+
+```
+cd CSSEHubShellClient
+cp punchin.sh /usr/local/bin/punchin
+chmod +rx /usr/local/bin/punchin
+```
+
+This will install the shell script in your local usr bin. Assuming you have /usr/local/bin on your $PATH, you can now punch in to CSSE hub by running `punchin` from a terminal.
+
+## Features
+Right now, all it can do is punch in to the CSSE Hub.
---
+++ punchin.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+DEFAULT_USERNAME=lokkesmoej
+
+#clean up session file on exit
+trap "rm -f cookiefile" EXIT
+
+printf "Login to CSSE Hub:\n"
+printf "Username: "
+read username
+
+# if no username is provided, use the defualt one
+if [ -z "$username" ]; then
+   username=$DEFAULT_USERNAME
+fi
+
+printf "Password: " 
+read -s password
+printf "\n"
+
+# Log in to CSSE hub
+res=$(curl -silent -L -c cookiefile -d "username=$username&password=$password&remember=false" https://xray.ion.uwplatt.edu/CSSE/Account/Login?returnUrl=)
+
+# if the response contains the word "incorrect"
+if [[ $res == *incorrect* ]]; then
+   printf "\nError: The username or password provided is incorrect\n"; exit 1
+fi
+
+res=$(curl -silent -L -b cookiefile -d "courseName=se4730&projectName=Project&inComment=&inPhase=Coding" http://xray.ion.uwplatt.edu/CSSE/Home/PunchIn)
+printf "\n"
+
+#if error punching
+if [[ $res != *\"status\":\"ok\"* ]]; then
+   printf "Error: "
+   echo $res | awk '{ if (match($0, /\"message\":\"[^\"]*\"/)) print substr($0, RSTART+11, RLENGTH-12); else print "Unknown error punching in." }'
+   exit 1
+fi
+
+printf "Punched in to CSSE Hub successfully\n"
+
