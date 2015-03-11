06db0b5eba886f4f542961d45073c50d051d3a83
Jake Lokkesmoe <jlokkesmoe@me.com>
2015-03-11 14:04:43 -0500

    Added auto log into IO and fixed some bugs

--- punchin.sh
+++ punchin.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-DEFAULT_USERNAME="Your_Username_Here"
+DEFAULT_USERNAME="lokkesmoej"
 
 #clean up session file on exit
 trap "rm -f cookiefile" EXIT
@@ -32,8 +32,13 @@ printf "\n"
 if [[ $res != *\"status\":\"ok\"* ]]; then
    printf "Error: "
    echo $res | awk '{ if (match($0, /\"message\":\"[^\"]*\"/)) print substr($0, RSTART+11, RLENGTH-12); else print "Unknown error punching in." }'
-   exit 1
+else
+   printf "Punched in to CSSE Hub successfully\n"
 fi
 
-printf "Punched in to CSSE Hub successfully\n"
+printf "\nLog in to IO? (y/n):"
+read doIo
 
+if [[ $doIo == *y* ]]; then
+   sshpass -p $password ssh $username@io.uwplatt.edu
+fi
